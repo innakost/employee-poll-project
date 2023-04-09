c2f4cbd73691004c58f6ecb3da57b842b4afc35d
Inna K <i.kostiuk@benjamin.co.jp>
2023-04-09 15:52:14 +0800

    add reducers & middleware

--- src/reducers/polls.js
+++ src/reducers/polls.js
@@ -1,6 +1,6 @@
 import { RECEIVE_POLLS } from "../actions/polls";
 
-export default function users(state = {}, action) {
+export function polls(state = {}, action) {
     switch (action.type) {
         case RECEIVE_POLLS:
             return {
